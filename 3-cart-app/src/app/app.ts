1cbb58907ec2c4c1c1bcb2afa6e25d409be60818
isaiah <antierrores10@gmail.com>
2025-09-15 19:58:30 -0500

    Shopping cart project start

---
+++ 3-cart-app/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CartApp } from './components/cart-app';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CartApp],
+  templateUrl: './app.html'
+})
+export class App {
+  protected readonly title = signal('3-cart-app');
+}
