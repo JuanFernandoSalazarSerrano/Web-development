9eb0f6d01a21ed6f05fe8c99e5416c687c1da10a
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-09-16 09:03:07 -0500

    cart now recieves items from items array and Added 0 products message in the shopping cart

--- 3-cart-app/src/app/components/cart/cart.ts
+++ 3-cart-app/src/app/components/cart/cart.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
+import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'cart',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './cart.html',
 })
 export class Cart {
